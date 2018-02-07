70a001beea9fa7457ad33127011aa9f340de2cc4
Andrii <andriisiuta@gmail.com>
2018-02-07 10:57:08 +0200

    validate second task and fixed if !integer

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,9 +1,12 @@
 let amounts = [];
 for(let i = 0; i < 2; i++) {
-    amounts.push(parseInt(prompt('Amount:')));
+    amounts.push(+(prompt('Amount:')));
 }
 
 [EURO, USD] = amounts;
+if(Number.isNaN(EURO) || Number.isNaN(USD)) {
+    console.warn('Incorrect data');
+}
 
 /*
 * @param {Number} Euro
@@ -13,8 +16,13 @@ for(let i = 0; i < 2; i++) {
 function convert(euro, usd) {
    let euroCourse = 34.5116;
    let usdCourse = 27.74240;
-   console.log(`${euro} euros are equal ${(euro * euroCourse).toFixed(2)}UAH,${usd} dollars are equal
-   ${(usd * usdCourse).toFixed(2)}UAH, one euro is equal ${(euroCourse/usdCourse).toFixed(2)} dollar`);
+   let euroToUah = euro * euroCourse;
+   let usdToUah = usd * usdCourse;
+   let euroToUsd = euroCourse / usdCourse;
+   usdToUah = Number.isInteger(usdToUah) ? usdToUah : usdToUah.toFixed(2);
+   euroToUah = Number.isInteger(euroToUah) ? euroToUah : euroToUah.toFixed(2);
+   euroToUsd = Number.isInteger(euroToUsd) ? euroToUsd : euroToUsd.toFixed(2);
+   console.log(`${euro} euros are equal ${(euroToUah)}UAH,${usd} dollars are equal ${usdToUah}UAH, one euro is equal ${euroToUsd} dollar`);
 }
 
 convert(EURO, USD);
