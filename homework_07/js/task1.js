fab1867de23037caf17ab43746aefb68baa4433d
Andrii <andriisiuta@gmail.com>
2018-02-09 11:18:02 +0200

    Creative first Task

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,40 +1,22 @@
-const elem = '[~]';
-const SPACE = ' ';
+let N = parseInt(prompt('Enter natural number N (0 < N <=20):'));
 
-const N = parseInt(prompt('Enter Value'));
-const Cells = 2 * N - 1;
-let SPACE_COUNT = (Cells - 1) / 2;
+pyramid = (N) => {
+    let SPACE_COUNT = N - 1;
+    let PYRAMID = '';
+    let elemCount = 1;
+    for(let i = 0; i < N; i++) {
+        if(i != N - 1) {
+            PYRAMID += ' '.repeat(SPACE_COUNT * 3);
 
-const PYRAMID = [];
-let end = Cells - 1;
-console.log('SPACE_COUNT:', SPACE_COUNT)
-
-// for(let i = 0; i < N; i++) {
-//     PYRAMID[i] = [];
-//     for(let j = 0; j < Cells; j++) {
-//         PYRAMID[i][j] = 0;
-//     }
-// }
-for(let i = 0; i < N; i++) {
-    PYRAMID[i] = [];
-    for(let j = 0; j < Cells; j++) {
-        for(let k = 0; k < SPACE_COUNT; k++) {
-            if(i !== N - 1) {
-                PYRAMID[i][k] = SPACE;
-                PYRAMID[i][end - k] = SPACE;
-            }
         }
-        if(PYRAMID[i][j] !== ' ' || i == N - 1) {
-            console.log('START')
-            console.log('--------------')
-            console.log(i,j)
-            console.log('--------------')
-            console.log('END')
-            PYRAMID[i][j] = elem;
-        }
+        PYRAMID += '[~]'.repeat(elemCount);
+        PYRAMID += '\n';
+        SPACE_COUNT--;
+        elemCount +=2;
     }
-}
 
-console.table(PYRAMID)
-// console.log(PYRAMID.join('\n'))
+    console.log(PYRAMID);
+};
+
+N = (Number.isInteger(N) && !Number.isNaN(N) && (N > 0 && N <= 20)) ? pyramid(N) : console.warn('Incorrect!');
 
