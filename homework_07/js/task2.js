0ba9f7ed8b73eb1468380f6686d5c914bc5c00d2
Andrii <andriisiuta@gmail.com>
2018-02-12 19:26:23 +0200

    change all

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,57 +1,45 @@
-game = (limit, startPrize, userPrize) => {
-    let winNumber = Math.floor(Math.random() * limit);
-    let userChoice;
-    console.log(winNumber)
-    let win = false;
-    let gamePrize = startPrize;
-    for (let i = 3; i > 0; i--) {
-        userChoice = prompt(`Choose a number in range [0: ${limit - 1}]
-                              Attempts left: ${i}
-                              totalPrize: ${userPrize}$
-                              Possible prize on current attempt: ${startPrize}$`);
-        if(!userChoice) {
-            break;
-        } else userChoice = +userChoice;
-        if (userChoice === winNumber && i > 0) {
-            userPrize += startPrize;
-            win = true;
-            break;
-        }
-        win = false;
-        startPrize = Math.floor(startPrize / 2);
-    }
-
-    if(win) {
-        let keepPlay = confirm(`Do u want to continue a game`);
-        limit = (limit * 2) - 1;
-        gamePrize *= 3;
-        keepPlay = keepPlay
-            ? game(limit, gamePrize, userPrize)
-            : console.log(`Thank you for a game. Your prize is ${userPrize}$`);
-        play('again');
-    } else {
-        console.log(`Thank you for a game. Your prize is ${userPrize}$`);
-        play('again');
-    }
-};
-
-play = (playAgain = 'game') => {
+let startPlay = confirm('Do u want to play a game?');
+if(startPlay) {
     let limit = 6;
     let userPrize = 0;
     let startPrize = 10;
-    let startPlay;
-    if(playAgain == 'game') {
-        startPlay = confirm(`Do u want to play a game?`);
-        if (startPlay) {
-            game(limit, startPrize, userPrize);
-        } else console.log('U did not become a millionaire');
-    } else if(playAgain == 'again') {
-        startPlay = confirm(`Do u want to play again?`);
-        if(startPlay) {
-            game(limit, startPrize, userPrize);
+    let gamePrize = startPrize;
+    do {
+        startPlay = false;
+        let winNumber = Math.floor(Math.random() * limit);
+        console.log(winNumber);
+        let userChoice;
+        let win = false;
+        for(let i = 3; i > 0; i--) {
+            userChoice = prompt(`Choose a number in range [0: ${limit - 1}]
+                                 Attempts left: ${i}
+                                 totalPrize: ${userPrize}$
+                                 Possible prize on current attempt: ${startPrize}$`);
+            if(!userChoice) {
+                startPrize = Math.floor(startPrize / 2);
+                continue;
+            } else userChoice = +userChoice;
+
+            if(userChoice == winNumber) {
+                userPrize += startPrize;
+                win = true;
+                break;
+            }
+            startPrize = Math.floor(startPrize / 2);
         }
-    }
-};
-
-play();
-
+        if(win) {
+            startPlay = confirm(`Do u want to continue a game`);
+            if(startPlay) {
+                limit = (limit * 2) - 1;
+                gamePrize *= 3;
+                startPrize = gamePrize;
+            } else {
+                console.log(`Thank u for a game. Your prize is ${userPrize}$`);
+                startPlay = confirm(`Do u want to play again?`);
+            }
+        } else {
+            console.log(`Thank u for a game. Your prize is ${userPrize}$ `);
+            startPlay = confirm(`Do u want to play again?`);
+        }
+    } while (startPlay);
+} else console.log(`U did not become a millionaire`);
