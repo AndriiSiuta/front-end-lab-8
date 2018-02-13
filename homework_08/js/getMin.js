4c2f56d079a8780fd19cc57aa58eb339113e1362
Andrii Siuta <andriisiuta@gmail.com>
2018-02-13 22:29:48 +0200

    Math.min ES6 without this context

--- homework_08/js/getMin.js
+++ homework_08/js/getMin.js
@@ -1,7 +1,1 @@
-function getMin () {
-    let min = 0;
-    for(let i = 0, len = arguments.length; i < len; i++) {
-        if(arguments[i] < min) min = arguments[i];
-    }
-    return min;
-}
+getMin = (...options) => Math.min(...options);
