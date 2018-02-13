5a2407c1fbe42c4fcaf66141d899af5e15523ad0
Andrii <andriisiuta@gmail.com>
2018-02-13 19:41:44 +0200

    2toDO and finish

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,6 +1,8 @@
+//toDo
 getClosestToZero = () => {
   let result;
   for(let i = 0, len = arguments.length; i < len; i++) {
-
   }
 };
+
+console.log(getClosestToZero(9, 5, -4, -9));
--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -5,6 +5,7 @@ function reverseNumber () {
         arguments[0] = parseInt(arguments[0] / 10);
     } while (arguments[0] > 0);
     console.log(result)
+    //toDo
     for(let i = 0, len = result.length; i < len ;i++) {
         console.log(result[i], result[i+1])
         if(result[i] == 0 && result[i+1] === 0) {
